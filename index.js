1a138946773590a7efb700ea4edd6869b046a09f
liban chana <libancw404@gmail.com>
2020-12-15 08:10:24 +0300

    added more variables

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-var maleName = ["kwasi", "kwandwo", "kwabena", "kwaku", "Yaw", "Koffi", "Kwame"]
-var femaleName = ["Akosua", "Adwao", "Abena", "Akua", "Yaa", "Afua", "Ama"]
+function dateSubmit() {
 
-function getday() {
+    let males = [" Kwasi", " Kwandwo", " Kwabena", " Kwaku", " Yaw", " Kofi", " Kwame"];
+    let females = [" Akosua", " Adwoa", " Abenaa", " Akua", " Yaa", " Afua", " Ama"];
 
-
-    var year = document.getElementById(" year").value;
-    var month = parseInt(document.getElementById(" month").value);
-    var day = parseInt(document.getElementById(" day").value);
-    var gender = parseInt(document.getElementById(" gender").value);
-    var clear = document.getElementById(" clear").reset;
-
-    var yy = parseInt(year.substr(2, 4));
-}
-
-function myFunction() {
-
-    let x, text;
+    let year = document.getElementById("year").value;
+    let month = parseInt(document.getElementById("month").value);
+    let day = parseInt(document.getElementById("day").value);
+    let gender = document.getElementById("gender").value;
+    let yy = parseInt(document.getElementById("year").value.substr(2, 4));
 
 
-    // Get the value of the input field with id="numb"
-    x = document.getElementById("ydate").value;
+    let century = parseInt(year.substr(0, 2));
 
-    // If x is Not a Number or less than one or greater than 10
-    if (isNaN(x) || x < 1 || x > 10) {
-        text = "Akan Name";
-    } else {
-        text = "Not Akan Name";
+    let date = Math.abs(parseInt(((century / 4) - 2 * century - 1) + ((5 * yy / 4)) + ((26 * (month + 1) /
+        10)) + day) % 7)
+
+
+    if (year <= 0 || year == "" || year > 2020) {
+        document.getElementById('name').innerHTML = ("enter valid year")
     }
-    document.getElementById("akan-name").innerHTML = text;
+    if (month == 2) {
+        if (day == 29) {
+            if (year % 4 != 0 || year % 100 == 0 && year % 400 != 0) {
+                prompt("enter valid date");
+                field.value = month + '/' + '';
+            }
+        } else if (day > 28) {
+            document.getElementById('name').innerHTML = ("enter valid date");
+            field.value = month + '/' + '';
+        }
+    } else
+    if (month <= 0 || month > 12) {
+        document.getElementById('name').innerHTML = ("enter valid month")
+    } else
+    if (day < 1 || day > 31) {
+        document.getElementById('name').innerHTML = ("enter valid date")
+
+    }
+
+    if (gender === "male") {
+        document.getElementById('name').innerHTML = (males[date])
+    } else
+    if (gender === "female") {
+        document.getElementById('name').innerHTML = (females[date])
+    }
 }
